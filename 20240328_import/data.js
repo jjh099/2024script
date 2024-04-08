1886c81a30a103b77b90676c2b90a51e3831bdda
jjh <wogml220723@naver.com>
2024-04-08 18:07:02 +0900

    first

---
+++ 20240328_import/data.js
@@ -0,0 +1,61 @@
+// const data = [
+//   {
+//     title: "안녕하세요",
+//     content: "ajhdjka",
+//     img: [{ filename: "img.JPG", originname: "han.jpg" }],
+//   },
+//   {},
+//   {},
+// ];
+
+// data[0].title;
+// data[0].content;
+// data[0].img[0].filename
+
+const data = [
+  {
+    idx: 0,
+    cata: "공지사항",
+    title: "[마감] 2024 넷마블견학 프로그램 참가 학교 및 기관 모집",
+    href: "http://www.naver.com",
+    content:
+      "넷마블문화재단은 게임 진로에 관심 있는 청소년을 대상으로 <넷마블견학프로그램> 참여 학교 및 기관을 모집합니다.",
+    date: "20204.03.06",
+    img: "testimg.jpg",
+  },
+  {
+    idx: 2,
+    cata: "공지사항",
+    title: "[마감] 2024 넷마블견학 프로그램 참가 학교 및 기관 모집",
+    href: "http://www.naver.com",
+    content:
+      "넷마블문화재단은 게임 진로에 관심 있는 청소년을 대상으로 <넷마블견학프로그램> 참여 학교 및 기관을 모집합니다.",
+    date: "20204.03.06",
+    img: "testimg.jpg",
+  },
+  {
+    idx: 3,
+    cata: "공지사항",
+    title: "[마감] 2024 넷마블견학 프로그램 참가 학교 및 기관 모집",
+    href: "http://www.naver.com",
+    content:
+      "넷마블문화재단은 게임 진로에 관심 있는 청소년을 대상으로 <넷마블견학프로그램> 참여 학교 및 기관을 모집합니다.",
+    date: "20204.03.06",
+    img: "testimg.jpg",
+  },
+  {
+    idx: 4,
+    cata: "공지사항",
+    title: "[마감] 2024 넷마블견학 프로그램 참가 학교 및 기관 모집",
+    href: "http://www.naver.com",
+    content:
+      "넷마블문화재단은 게임 진로에 관심 있는 청소년을 대상으로 <넷마블견학프로그램> 참여 학교 및 기관을 모집합니다.",
+    date: "20204.03.06",
+    img: "testimg.jpg",
+  },
+];
+
+const testData = "안녕";
+export default data; //하나만 뱉어낼때
+
+// export { data, testData }; //두개이상 뱉어낼때
